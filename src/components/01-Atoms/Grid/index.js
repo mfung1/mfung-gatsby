d33db5b3acc1888a139780a65bffd47e6f3ade4b
ocec <matthew.fung@tesco.com>
2019-12-24 16:26:27 +0000

    Add grid

---
+++ src/components/01-Atoms/Grid/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './grid.module.css';
+
+const Grid = (props) => {
+  return (
+    <div className={`${styles.grid} ${styles[`grid-${props.split}`]} ${props.stampGroup ? styles['grid-stampGroup'] : ''} ${styles[`grid-gap-${props.gap}`]} mb-${props.spacing}`}>
+      {props.children}
+    </div>
+  );
+}
+ 
+export default Grid;

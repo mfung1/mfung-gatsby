15cf47458615d9c6b4310b09c4deb68b5c26184e
uk80109967 <matthew.fung@tesco.com>
2020-08-05 16:30:29 +0100

    Add icons and icon group css

--- src/components/03-Organisms/Hero/hero.js
+++ src/components/03-Organisms/Hero/hero.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Heading from '../../01-Atoms/Typography/Headings';
 import MFLink from '../../02-Molecules/Link';
+import Icon from '../../01-Atoms/Icon';
 
 
 const Hero = () => {
@@ -8,6 +9,12 @@ const Hero = () => {
     <section className="mf-hero">
       <div className="mf-hero_copy">
         <Heading level="h1" colour="clearDay" spacing="mb-11" text={<>Hi, I’m Matt Fung.<br/>A British web developer, currently working at <MFLink external path="https://www.tesco.com" classNames="col-snowyMint mf-h1 bolder" text="Tesco"/>.</>}/>
+      </div>
+      <div className="mf-icons">
+        <Icon type="twitter"/>
+        <Icon type="github"/>
+        <Icon type="medium"/>
+        <Icon type="email"/>
       </div>
     </section>
   );
